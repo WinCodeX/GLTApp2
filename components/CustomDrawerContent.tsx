e8d4c603938fc8dc15c3d0bddbc1b319095a6067
WinCodeX <glenwinterg970@gmail.com>
2025-10-25 16:00:13 +0300

    fix

--- components/CustomDrawerContent.tsx
+++ components/CustomDrawerContent.tsx
@@ -628,7 +628,7 @@ export default function CustomDrawerContent(props: any) {
 
           {/* Invite Friends */}    
           <DrawerItem    
-            label="Invite a Friend"    
+            label="Invite Friends"    
             labelStyle={styles.label}    
             icon={() => <Feather name="user-plus" size={24} color={colors.primary} />}    
             onPress={async () => {
